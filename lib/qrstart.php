3568250d5819e751691d360dc75ca0d1b4759fcd
foldas <web@foldina.cz>
2021-01-07 00:20:17 +0100

    PHP Library

---
+++ lib/qrstart.php
@@ -0,0 +1,52 @@
+<?php
+class QrStart {
+	protected $apiKey;
+	public function __construct($apiKey=NULL) {
+		$this->apiKey = $apiKey;
+	}
+	public function qrCode($params=NULL) {
+		if (empty($params)) {
+			$result=[
+				'success' => false,
+				'message' => 'empty params'
+			];
+		} else {
+			$result = $this->startCode(array_merge($params, ['apikey' => $this->apiKey]));
+		}
+		return json_encode($result);
+	}
+	public function startCode($data=NULL) {
+		$ch=curl_init();
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_URL, 'https://api.qrstart.eu');
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array('Accept: application/json', 'Content-Type: application/json'));
+		curl_setopt($ch, CURLOPT_POST, true);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+		curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+		$response = curl_exec($ch);
+		if (curl_errno($ch)) {
+			$curl_errno = curl_error($ch);
+		}
+		curl_close($ch);
+		if (isset($curl_errno)) {
+			$result=[
+				'success' => false,
+				'message' => $curl_errno
+			];
+		} else {
+			$transform = json_decode($response, true);
+			if ($transform['success'] === true) {
+				$result=[
+					'success' => true,
+					'image' => $transform['image']
+				];
+			} else {
+				$result=[
+					'success' => false,
+					'message' => $transform['message']
+				];
+			}
+		}
+		return $result;
+	}
+}
